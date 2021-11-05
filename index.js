15ad5350f689d25908570e3ea95d45f7a95e0eb6
Marcel Perin <mapirpg@gmail.com>
2021-11-04 21:24:34 -0300

    !!!!!!!!!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const catalogo = (require('./database/catalogo.json'))
 
+console.log('!!!!!!!!!!!!!!!!!!')
+
 adicionarFilme = (titulo, duracao, atores, anoDeLancamento, emCartaz) => {
     catalogo.push({
         codigo: catalogo.length,
